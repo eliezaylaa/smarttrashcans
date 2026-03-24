f572752969eafc4889d3e8594154bef053463e2d
eliezaylaa <eliezaylaa@gmail.com>
2026-03-25 00:28:30 +0100

    feat(arduino): game2 - sequential LEDs

--- arduino/game2/ledtest.ino/ledtest.ino.ino
+++ arduino/game2/ledtest.ino/ledtest.ino.ino
@@ -1,37 +1,16 @@
 void setup() {
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12,OUTPUT);
 }
 
 void loop() {
-  // fade LED1 up
-  for (int i = 0; i <= 255; i++) {
-    analogWrite(11, i);
-    delay(5);
-  }
-  // fade LED1 down
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(11, i);
-    delay(5);
-  }
-  for (int i = 0; i <= 255; i++) {
-    analogWrite(10, i);
-    delay(5);
-  }
-  // fade LED1 down
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(10, i);
-    delay(5);
-  }
-  for (int i = 0; i <= 255; i++) {
-    analogWrite(9, i);
-    delay(5);
-  }
-  // fade LED1 down
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(9, i);
-    delay(5);
-  }
-  // repeat for LED2 and LED3...
-}
+ digitalWrite(13,HIGH);
+ delay(1000);
+ digitalWrite(13,LOW);
+ delay(1000);
+ digitalWrite(12,HIGH);
+ delay(500);
+ digitalWrite(12,LOW);
+ delay(500);
+} 
+
